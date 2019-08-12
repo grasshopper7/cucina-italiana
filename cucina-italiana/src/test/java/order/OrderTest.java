21098142c9feae76a72eb120626690040ee59332
grasshopper7 <grass.hopper.moc@gmail.com>
2019-08-12 15:57:11 +0530

    Reports

--- cucina-italiana/src/test/java/order/OrderTest.java
+++ cucina-italiana/src/test/java/order/OrderTest.java
@@ -2,9 +2,11 @@ package order;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"html:target/html","json:target/json/report.json","junit:target/junit/report.xml"})
 public class OrderTest {
 
 }
