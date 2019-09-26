5cb52aaa76adbc6890617241d8b061ad84a967dc
grasshopper7 <grass.hopper.moc@gmail.com>
2019-09-26 10:10:38 +0530

    Add summary and progress plugins

--- cucina-italiana/src/test/java/order/OrderTest.java
+++ cucina-italiana/src/test/java/order/OrderTest.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;;
 
-
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/html","json:target/json/report.json","junit:target/junit/report.xml"})
+@CucumberOptions(plugin = { "html:target/html", "json:target/json/report.json", "junit:target/junit/report.xml",
+		"summary", "progress" })
 public class OrderTest {
 
 }
